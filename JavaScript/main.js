abc483d0f20dd360a01edda8d6440c0a332d8c62
cddavidson <christopher_davidson@alumni.brown.edu>
2024-04-10 20:27:09 -0500

    I added 4 paragraphs of placeholder text and two JS buttons to the home page.

---
+++ JavaScript/main.js
@@ -0,0 +1,28 @@
+function greetUser() {
+    const now = new Date();
+    var current_time = now.getHours();
+    var greeting;
+
+    if (current_time < 12) {
+        greeting = "Good Morning!"
+    } else if (current_time < 18) {
+        greeting = "Good Afternoon!"
+    } else {
+        greeting = "Good Evening!"
+    }
+
+    alert(greeting);
+}
+
+function changeButton() {
+    let button = document.getElementById("button_2");
+    console.log("Before Change:", button.textContent, button.className);
+
+    button.textContent = "Done";
+    button.className = "btn btn-sucess";
+
+    console.log("After Change:", button.textContent, button.className);
+}
+
+document.getElementById("button_1").addEventListener("click", greetUser);
+document.getElementById("button_2").addEventListener("click", changeButton);
