d9e808b6dc90a9efbe0b62031db751a535fc1ab4
cddavidson <christopher_davidson@alumni.brown.edu>
2024-04-28 21:38:26 -0500

    One graph is done.

--- JavaScript/wk_6_linechart.js
+++ JavaScript/wk_6_linechart.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
   const ctx = document.getElementById('lineChart').getContext('2d');
-  const labels = Utils.months({count: 12});
   new Chart(ctx, {
     type: 'line',
     data: {
-      labels: labels,
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
       datasets: [{
-        label: 'Average Monthly Temperatures (in Fahrenheit)',
+        label: 'Average Monthly Temperature (in Fahrenheit)',
         data: [22, 18, 40, 52, 67, 76, 88, 82, 63, 55, 51, 38],
         backgroundColor: 'rgb(224, 255, 255)',
         borderColor: 'rgb(75, 192, 192)',
